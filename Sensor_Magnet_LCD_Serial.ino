7903cce07357c09e43c03a4fcde917a4edf8d69e
sebastien gaillac <sebastien.gaillac@outlook.com>
2016-03-31 14:29:22 +0200

    Rename Sensor_Magnet_LCD_Serial.JPG.ino to Sensor_Magnet_LCD_Serial.ino

---
+++ Sensor_Magnet_LCD_Serial.ino
@@ -0,0 +1,191 @@
+/*
+Projet: mise en oeuvre; - Cap d'un bateau 
+ Module Boussole HMC5883L 
+ Communication Arduino>I2C>LCD
+ Fait le 30/03/2016 par Gaillac Sébastien
+*/
+
+#include <Wire.h>   //Librairie Wire pour la communication I2C
+#define HMC5803L_Address 0x1E  //Adresse I2C du module Hmc5883l
+#include <LiquidCrystal.h> //Librairie LCD
+
+#define X 3  //Adresses de registres pour les données X Y et Z
+#define Y 7
+#define Z 5
+
+// Déclarer des nombres a firgule, meme chose que la fonction "FLOAT"
+double Xmagnetic;
+double Ymagnetic;
+double Zmagnetic;
+double Module_magnetic;
+double angle;
+
+LiquidCrystal lcd( 12, 11, 9, 8, 7, 6 ); 
+
+
+void setup() 
+{
+
+  Serial.begin(9600);
+  Wire.begin();         //Initialisation de la livrairie Wire
+   Init_HMC5803L();    //Initialiser le module boussole
+   lcd.begin(24, 4);
+   lcd.clear();  //s'assurer que l'ecran est vide
+   lcd.print("Boussole Paree");
+   delay(1000);
+}
+
+// Boucle de mesures de champ magnétique dans le moniteur ainsi que sur lcd
+void loop() 
+{
+  Xmagnetic = HMC5803L_Read(X);  //lecture sur 3 axes et sortie sur le port sériel
+  Ymagnetic = HMC5803L_Read(Y);
+  Zmagnetic = HMC5803L_Read(Z);
+  Serial.print(" ");
+  Serial.print (Xmagnetic);
+  Serial.print(" ");
+  Serial.print (Ymagnetic);
+  Serial.print(" ");
+  Serial.print (Zmagnetic);
+
+//Module du champ
+    Module_magnetic = Xmagnetic * Xmagnetic + Ymagnetic * Ymagnetic + Zmagnetic * Zmagnetic;
+    Module_magnetic = sqrt(Module_magnetic);
+    Serial.print(" M= ");
+    Serial.print(Module_magnetic);
+  
+//Calculer l'angle de la boussole à partir de X et Y (à plat)
+    angle= atan2(Ymagnetic,Xmagnetic) * (180 / 3.14159265); // angle en degres
+if (angle<0) {angle=angle+360;}
+   Serial.print("  ");  
+   Serial.print("Angle  ");  //en degres
+   Serial.println(angle);
+  
+if (angle > 22 && angle < 67)
+  {
+   Serial.print("Nord Est");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Nord Est");
+  }
+
+ if ( angle > 112 && angle < 157)
+  {
+   Serial.print("Sud Est");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Sud Est");
+  }
+
+ if (angle > 202 && angle < 247)
+  {
+   Serial.print("Sud Ouest");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Sud Ouest");
+  }
+
+if (angle > 292 && angle < 337)
+  {
+   Serial.print("Nord Ouest");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Nord Ouest");
+  }
+
+ if (angle >= 337 || angle <= 22)
+  {
+   Serial.print("Nord");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Nord");
+  }
+
+ if (angle >= 67 && angle <=112)
+  {
+   Serial.print("Est");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Est");
+  }
+  
+ if (angle >= 157 && angle <=202)
+  {
+   Serial.print("Sud");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Sud");
+  }
+  
+ if (angle >= 247 && angle <=292)
+  {
+   Serial.print("Ouest");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("angle:");
+    lcd.print(angle);
+    lcd.setCursor(10,0);
+    lcd.print("Ouest");
+  }
+ delay(500);
+}
+
+// === Fonction qui initialise le module boussole (à lancer une seule fois)
+void Init_HMC5803L(void)
+{
+  /* Set the module to 8x averaging and 15Hz measurement rate */
+  Wire.beginTransmission(HMC5803L_Address);
+  Wire.write(0x00);
+  Wire.write(0x70);
+         
+  Wire.write(0x01);
+  Wire.write(0xA0);   //Règle un gain de 5
+  Wire.endTransmission();
+}
+
+// === Fonction qui lit le module boussole (registre d'un des 3 axes, retourne 16 bits)
+int HMC5803L_Read(byte Axis)
+{
+  int Result;
+   /* Initiate a single measurement */
+  Wire.beginTransmission(HMC5803L_Address);
+  Wire.write(0x02);
+  Wire.write(0x01);
+  Wire.endTransmission();
+  delay(6);
+  
+  /* Move modules the resiger pointer to one of the axis data registers */
+  Wire.beginTransmission(HMC5803L_Address);
+  Wire.write(Axis);
+  Wire.endTransmission();
+   
+  /* Read the data from registers (there are two 8 bit registers for each axis) */  
+  Wire.requestFrom(HMC5803L_Address, 2);
+  Result = Wire.read() << 8;
+  Result |= Wire.read();
+
+  return Result;
+}
+
+ 
